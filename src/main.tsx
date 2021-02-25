7b4677207c64b072b4e814e72cb3b9ffc9100998
ruritoBlogger <rurito.blog@gmail.com>
2021-02-25 12:07:59 +0900

    feat: 画面サイズを調整するコードを別ファイルに分離

--- src/main.tsx
+++ src/main.tsx
@@ -1,54 +1,63 @@
-import * as THREE from 'three';
-import {useState, useEffect} from 'react';
+import { WebGLRenderer, Scene, AmbientLight, PerspectiveCamera } from "three";
+
+import useGetWindowSize from "./useGetWindowSize";
 
 const Main: React.FC = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
+  const { width, height } = useGetWindowSize();
 
-    const createBox = (width: number, height: number) => {
-        // レンダラを作成
-        const renderer: any = new THREE.WebGLRenderer({
-          canvas: document.querySelector("#canvas") as HTMLCanvasElement
-        });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(width, height);
-        // シーンを作成
-        const scene = new THREE.Scene();
-        // カメラを作成
-        const camera = new THREE.PerspectiveCamera(45, width / height);
-        camera.position.set(0, 0, +1000);
-        // 箱を作成
-        const geometry = new THREE.BoxGeometry(400, 400, 400);
-        const material = new THREE.MeshNormalMaterial(); const box = new THREE.Mesh(geometry, material);
-        scene.add(box);
-        tick();
-        // 毎フレーム時に実行されるループイベント
-        function tick() {
-          box.rotation.y += 0.01;
-          renderer.render(scene, camera);
-          // レンダリング
-          requestAnimationFrame(tick);
-        }
-      };
+  /**
+   * MMDモデルを読み込みThree.jsを用いて描画を行う
+   * @param canvas MMDモデルを描画する先
+   */
+  const createCharactor = (canvas: HTMLCanvasElement) => {
+    // シーンの作成
+    const scene = new Scene();
+    const renderer = new WebGLRenderer({ canvas: canvas, antialias: true });
 
-      useEffect(() => {
-        createBox(width, height);
-      }, [width, height])
+    // 光の作成
+    var ambient = new AmbientLight(0xeeeeee);
+    scene.add(ambient);
 
-      useEffect(() => {
-        const onResize = () => {
-          setWidth(window.innerWidth);
-          setHeight(window.innerHeight);
-        }
-        window.addEventListener('resize', onResize);
-        return () => window.removeEventListener('resize', onResize);
-      }, []);
+    // 画面表示の設定
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xcccccc, 0);
+    document.body.appendChild(renderer.domElement);
 
-    return (
-      <>
-        <canvas id="canvas" />
-      </>
-    )
-}
+    // カメラの作成
+    camera.position.set(0, 10, 60);
+
+    // モデルとモーションの読み込み準備
+    var modelFile = "./mmd/miku/Lat式ミクVer2.31_Normal.pmd";
+    var onProgress = function (xhr) {};
+    var onError = function (xhr) {
+      console.log("load mmd error");
+    };
+    //MMDLoaderをインスタンス化
+    var loader = new MMDLoader();
+    //loadModelメソッドにモデルのPATH
+    //コールバックに画面に描画するための諸々のプログラムを書く
+    loader.loadModel(
+      modelFile,
+      function (object) {
+        mesh = object;
+        mesh.position.set(0, -10, 0);
+        mesh.rotation.set(0, 0, 0);
+        scene.add(mesh);
+      },
+      onProgress,
+      onError
+    );
+
+    // リサイズ時
+    window.addEventListener("resize", onWindowResize, false);
+  };
+
+  return (
+    <>
+      <canvas id="canvas" />
+    </>
+  );
+};
 
 export default Main;
